b388278167821218e6ee8d0fd0ae3bca2e577462
Tony Chen <theundertaker@126.com>
2021-11-01 17:54:58 +0800

    1

---
+++ Python/Study/Base/Print/Print.py
@@ -0,0 +1,53 @@
+print('Single Quotes')
+print("Double Quotes")
+
+# name = input('Please enter your name: ')
+# print(name)
+
+#Printing blank lines
+print('Hellow  world')
+print()
+print('Check the blank line')
+print('Blank line \nin the middle of string') 
+
+# Using double quotes for this string 
+# Because the string itself contains a singke quote
+print("It's a small world after all")
+
+import os
+print('Hello, ' + os.getlogin() + '! How are you?')
+print(f'Hello, {os.getlogin()}! How are you?')
+
+a=1
+b=2
+print('My age is ' + str(a+b))
+
+
+# >>> print(42)                            # <class 'int'>
+# 42
+
+# >>> print(3.14)                          # <class 'float'>
+# 3.14
+
+# >>> print(1 + 2j)                        # <class 'complex'>
+# (1+2j)
+
+# >>> print(True)                          # <class 'bool'>
+# True
+
+# >>> print([1, 2, 3])                     # <class 'list'>
+# [1, 2, 3]
+
+# >>> print((1, 2, 3))                     # <class 'tuple'>
+# (1, 2, 3)
+
+# >>> print({'red', 'green', 'blue'})      # <class 'set'>
+# {'red', 'green', 'blue'}
+
+# >>> print({'name': 'Alice', 'age': 42})  # <class 'dict'>
+# {'name': 'Alice', 'age': 42}
+
+# >>> print('hello')                       # <class 'str'>
+# hello
+
+
